4ba6e02be45739b381b9fbaea3e8be0855362779
unknown <ellayab804@gmail.com>
2025-06-07 04:36:56 +0300

    feat: smooth animation and sound added

--- src/seat-selection/index.tsx
+++ src/seat-selection/index.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { HandicapIcon, NoHandicapIcon, TriangleIcon } from "../assets";
 import Seat from "../components/Seat";
@@ -7,12 +8,29 @@ import allSeats from "../seats";
 import planeLayout from "/images/main-plane-body.png";
 import planeWingRight from "/images/right-wing.png";
 import planeWingLeft from "/images/left-wing.png";
+import planeSound from "/audio/plane-flight.mp3";
+
+const containerVariants = {
+  animate: {
+    transition: {
+      staggerChildren: 0.3,
+      delayChildren: 0.2,
+    },
+  },
+};
+
+const itemVariants = {
+  initial: { opacity: 0, scale: 0.95 },
+  animate: { opacity: 1, scale: 1, transition: { duration: 0.6 } },
+  exit: { opacity: 0, scale: 0.8, transition: { duration: 0.4 } },
+};
 
 export default function SeatSelection() {
   const [selectedSeats, setSelectedSeats] = useState<string[]>([]);
-  const [selectionAmount, setSelectionAmount] = useState<number>(1);
+  const [selectionAmount, setSelectionAmount] = useState<number>(2);
   const [tooltipSeat, setTooltipSeat] = useState<string | null>(null);
   const [exitPlane, setExitPlane] = useState(false);
+  const [showStartText, setShowStartText] = useState(false);
   const tooltipTimeoutRef = useRef<NodeJS.Timeout | null>(null);
   //   const isSmallDevice = useScreenWidthMatch();
 
@@ -61,9 +79,25 @@ export default function SeatSelection() {
     setSelectionAmount(amount);
   }
 
+  function handleRestart() {
+    setShowStartText(false);
+    setSelectedSeats([]);
+    setTimeout(() => {
+      setExitPlane(false);
+      playSound(planeSound);
+    }, 500);
+  }
+
+  function playSound(src: string) {
+    const audio = new Audio(src);
+    audio.play();
+  }
+
   return (
     <div className="mx-auto pb-10 pt-2 w-fit">
-      <div className="sticky top-0 px-4 pt-2 sm:pt-6 w-full max-w-[1200px] mx-auto gap-y-2 flex flex-col sm:gap-y-4">
+      <div
+        className={`${exitPlane ? "sticky" : "relative"} w-full top-0 px-4 pt-2 sm:pt-6 max-w-[1200px] mx-auto gap-y-2 flex flex-col sm:gap-y-4`}
+      >
         {/* 1. Seat Status Legend */}
         <div className="flex flex-wrap justify-center gap-2 xs:gap-4 sm:gap-6 bg-white shadow-sm border rounded-lg py-3 px-2 sm:py-4">
           <SeatDescription color="bg-[#A8D5BA]" label="Available" />
@@ -128,79 +162,151 @@ export default function SeatSelection() {
           >
             Clear Selection
           </button>
-          <button
-            className="group bg-green-600 flex items-center gap-2 text-white px-5 py-2 rounded-md hover:bg-green-500 transition active:scale-[0.98]"
-            onClick={() => {
-              setExitPlane((cur) => !cur);
-            }}
-          >
-            Submit
-            <IoIosArrowRoundForward className="text-2xl group-hover:translate-x-2 duration-500" />
-          </button>
+          {selectedSeats.length > 0 && (
+            <button
+              className={`group bg-green-600 flex items-center gap-2 text-white px-5 py-2 rounded-md  transition ${!exitPlane && "active:scale-[0.98] hover:bg-green-500"}`}
+              onClick={() => {
+                setExitPlane(true);
+                setSelectionAmount(2);
+                playSound(planeSound);
+                setTimeout(() => {
+                  setShowStartText(true);
+                }, 1500); // matches exit animation duration
+              }}
+              disabled={exitPlane}
+            >
+              Submit
+              <IoIosArrowRoundForward
+                className={`text-2xl ${!exitPlane && "group-hover:translate-x-2"} duration-500`}
+              />
+            </button>
+          )}
         </div>
       </div>
 
       {/* Plane Layout Seat Selection */}
-      <div
-        className={`mx-auto w-[calc(100vw-14px)] overflow-hidden relative mt-10 ${exitPlane ? "-translate-y-[calc(100%+500px)]" : "translate-y-0"} duration-[2000ms] transition-all ease-in`}
-      >
-        <div className="relative xxs:max-w-[398px] xs:max-w-[398px] sm:max-w-[456px] mx-auto overflow-x -scroll">
-          {/* Plane Body Image */}
-          <img
-            src={planeLayout}
-            alt="plane"
-            className=" w-full object-contain"
-          />
-          {/* Plane Left Wing */}
-          <div className="absolute left-0 top-[35%] overflow">
-            <img
-              src={planeWingLeft}
-              alt=""
-              className="w-full -translate-x-full object-contain"
-            />
-          </div>
-          {/* Plane Right Wing */}
-          <div className="absolute right-0 top-[35%] overflow">
-            <img
-              src={planeWingRight}
-              alt=""
-              className="w-ful translate-x-full object-contain"
-            />
-          </div>
-        </div>
-
-        {/* Seats grid */}
-        <div className="absolute space-y-1 sm:space-y-2 right-1/2 translate-x-1/2 top-[23%]">
-          {allSeats.map((columnSeat, i) => (
-            <div key={i} className="flex w-fit">
-              {columnSeat.map((rowSeat, j) =>
-                rowSeat ? (
-                  rowSeat.hideSeat ? (
-                    <div className="w-[13vw] xxs:w-[13vw] xs:w-14 sm:w-16" />
-                  ) : (
-                    <Seat
-                      key={j}
-                      seatData={rowSeat}
-                      onClick={() =>
-                        rowSeat.status === "available" &&
-                        toggleSeatSelection(rowSeat.id)
-                      }
-                      isSelected={selectedSeats.includes(rowSeat.id)}
-                      tooltipMessage={
-                        tooltipSeat === rowSeat.id
-                          ? "To select this seat, deselect another"
-                          : null
-                      }
-                    />
-                  )
-                ) : (
-                  <div key={j} className="w-6 sm:w-8" />
-                )
-              )}
+      <AnimatePresence mode="wait">
+        {!exitPlane && (
+          <motion.div
+            key="plane-layout"
+            initial={{ y: "100vh", opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: {
+                type: "spring",
+                stiffness: 50,
+                damping: 16,
+                duration: 2.5,
+                ease: "easeInOut",
+              },
+            }}
+            exit={{
+              y: "-250vh",
+              opacity: 1,
+              transition: { duration: 2, ease: "easeInOut" },
+            }}
+            className={`mx-auto w-[calc(100vw-18px)] sm:w-[calc(100vw-14px)] overflow-hidden relative mt-10 `}
+          >
+            <div className="relative xxs:max-w-[398px] xs:max-w-[398px] sm:max-w-[456px] mx-auto">
+              {/* Plane Body Image */}
+              <img
+                src={planeLayout}
+                alt="plane"
+                className=" w-full object-contain"
+              />
+              {/* Plane Left Wing */}
+              <div className="absolute left-0 top-[35%] overflow">
+                <img
+                  src={planeWingLeft}
+                  alt=""
+                  className="w-full -translate-x-full object-contain"
+                />
+              </div>
+              {/* Plane Right Wing */}
+              <div className="absolute right-0 top-[35%] overflow">
+                <img
+                  src={planeWingRight}
+                  alt=""
+                  className="w-ful translate-x-full object-contain"
+                />
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
+
+            {/* Seats grid */}
+            <div className="absolute space-y-1 sm:space-y-2 right-1/2 translate-x-1/2 top-[23%]">
+              {allSeats.map((columnSeat, i) => (
+                <div key={i} className="flex w-fit">
+                  {columnSeat.map((rowSeat, j) =>
+                    rowSeat ? (
+                      rowSeat.hideSeat ? (
+                        <div className="w-[13vw] xxs:w-[13vw] xs:w-14 sm:w-16" />
+                      ) : (
+                        <Seat
+                          key={j}
+                          seatData={rowSeat}
+                          onClick={() =>
+                            rowSeat.status === "available" &&
+                            toggleSeatSelection(rowSeat.id)
+                          }
+                          isSelected={selectedSeats.includes(rowSeat.id)}
+                          tooltipMessage={
+                            tooltipSeat === rowSeat.id
+                              ? "To select this seat, deselect another"
+                              : null
+                          }
+                        />
+                      )
+                    ) : (
+                      <div key={j} className="w-6 sm:w-8" />
+                    )
+                  )}
+                </div>
+              ))}
+            </div>
+          </motion.div>
+        )}
+
+        {showStartText && (
+          <motion.div
+            key="start-again-text"
+            variants={containerVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="flex h-[calc(100vh-300px)] sm:h-[calc(100vh-300px)] flex-col items-center justify-center"
+          >
+            <motion.h1
+              variants={itemVariants}
+              className="text-gray-700 text-4xl font-semibold text-center mb-6"
+            >
+              Reservation Complete
+            </motion.h1>
+
+            <motion.h2
+              variants={itemVariants}
+              className="text-green-700 text-2xl sm:text-3xl font-bold text-center"
+            >
+              You're all set!
+            </motion.h2>
+
+            <motion.p
+              variants={itemVariants}
+              className="text-slate-500 text-lg sm:text-xl text-center mb-6"
+            >
+              Want to make another reservation?
+            </motion.p>
+
+            <motion.button
+              variants={itemVariants}
+              onClick={handleRestart}
+              className="bg-blue-500 hover:bg-blue-500/90 text-white px-8 py-2 rounded-lg transition-all"
+            >
+              Start Again
+            </motion.button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
